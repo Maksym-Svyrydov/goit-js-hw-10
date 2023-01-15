cd13728d364a83c332a82d865f81a9df403920ab
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-01-15 11:37:02 +0100

    v1.3

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASIC_URL = 'https://restcountries.com/v3.1/name';
 const options = 'name,capital,population,flags,languages';
 
-const fetchCountries = name => {
+function fetchCountries(name) {
   return fetch(`${BASIC_URL}/${name}?fields=${options}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
